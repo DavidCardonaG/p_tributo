6ec03bd380a3cdb4600626e03dd03183f6fa7de4
DavidCardonaG <softwarewd@outlook.com>
2021-08-22 22:00:23 -0500

    Add estructura Js

---
+++ data/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+    "bio": "1989: Nacio en la ciudad de Dortmund"
+    },
+    {
+    "bio": "2000: Ingreso a las cannteras del Borussia Dortmund."
+    },
+    {
+    "bio": "2005:Cambio al equipo Sub 17 del Borussia Dortmund para formar parte de la cantera del Rot Weiss Ahlen."
+    },
+    {
+    "bio": "2008: Debutó a los 19 años como profesional en el Rot Weiss Ahlen."
+    },
+    {
+    "bio": "2009: El 25 de mayo firmo contrato por 4 años con el Borussia Mönchengladbach."
+    },
+    {
+    "bio": "2011: Debut en la selección absoluta de alemania."
+    },
+    {
+    "bio": "2012: El 24 de agosto debuto con el Borussia Dortmund en casa ante el SV Werder Bremen."
+    },
+    {
+    "bio": "2013: Lesión de los ligamentos del tobillo que lo dejo fuera del mundial de brasil 2014 en el cual salieron campeones los alemanes."
+    },
+    {
+    "bio": "2014-2016: Dureante esta epoca tuvo varias lesiones de importancia que lo mantuvieron apartado de las canchas de juego."
+    },
+    {
+    "bio": "2017: El 27 de mayo disputó la final de la copa alemana ante el  Eintracht Frankfurt, en el cual pudo salir campeón y tener su primer título de prestigio."
+    },
+    {
+    "bio": "2018: Ganó su cuarto título con el Borussia Dortmund contra el Bayern Múnich."
+    },
+    {
+    "bio": "2021: Máximo goleador del Borussia Dortmund en la liga de campeones con 18 tantos."
+    }
+]
---
+++ script/scripts.js
@@ -0,0 +1,34 @@
+import {data} from '../data/data.js';
+
+const printList = document.getElementById('lista');
+const fragment = document.createDocumentFragment();
+let button = document.getElementById('btn');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadList(data);
+})
+
+const loadList = (data) => {
+    data.forEach(biografia => {
+        const{bio} = biografia;
+        printList.textContent = bio;
+        const clone = printList.cloneNode(true);
+        fragment.appendChild(clone)
+    });
+    printList.appendChild(fragment);
+}
+
+localStorage.setItem('biografia', JSON.stringify(data));
+
+
+// function confirm(){
+//     let res = ''
+//     if(res == true){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+
